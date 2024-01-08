91137be27e5ebe6dced1cc71962bda17e249364a
TommasoTodescato <tommasotodescato@gmail.com>
2024-01-08 21:29:09 +0100

    b

---
+++ gioco_impiccato/include/player.h
@@ -0,0 +1,12 @@
+#ifndef GIOCO_IMPICCATO_PLAYER_H
+#define GIOCO_IMPICCATO_PLAYER_H
+
+namespace gioco
+{
+	class player
+	{
+
+	};
+}
+
+#endif
--- gioco_impiccato/main.cpp
+++ gioco_impiccato/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
-
-
+#include <fstream>
+#include <string>
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	std::ofstream myfile ("/home/tommaso/info/tpsit/gioco_impiccato/parole.txt", std::ios::app);
+	if (!myfile.is_open())
+	{
+		std::cout << "Percorso non valido" << std::endl;
+		return 1;
+	}
+
+	myfile << "caca\n";
+	myfile << "popo\n";
+	myfile.close();
+
+
 	return 0;
 }
