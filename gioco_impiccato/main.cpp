69677f9f761fd83c25dc764d2a5f8fc0b7d31acc
TommasoTodescato <tommasotodescato@gmail.com>
2024-01-09 08:57:53 +0100

    game works

--- gioco_impiccato/main.cpp
+++ gioco_impiccato/main.cpp
@@ -28,22 +28,57 @@ string get_random_word(const string& file_path, const int& file_length)
 	return s;
 }
 
-
 int main()
 {
+	int tries = 10;
+
 	string word = get_random_word("/home/tommaso/info/tpsit/gioco_impiccato/parole.txt", FILE_LEN);
 	cout << word << endl;
 
-	ofstream myfile ("/home/tommaso/info/tpsit/gioco_impiccato/a.txt", ios::app);
-	if (!myfile.is_open())
+	string unknown(word.size(), '_');
+	for (int i = 0; i < tries; i++)
 	{
-		cout << "Percorso non valido" << endl;
-		return 1;
+		cout << "[?] Indovina la lettera: " << endl;
+
+		string input;
+		cin >> input;
+		while (input.length() != 1 || !isalpha(input[0]))
+		{
+			cout << "[!] Stringa non alfabetica o troppo lunga. Riprova: " << endl;
+			cin >> input;
+		}
+
+		if (word.find(input[0]) != string::npos)
+		{
+			for (int c = 0; c < unknown.length(); c++)
+				if (word[c] == input[0])
+					unknown[c] = word[c];
+			cout << "[V] Giusto | " << unknown << endl;
+		}
+		else
+			cout << "[X] Sbagliato | " << unknown << endl;
+
+		if (word == unknown)
+		{
+			cout << "[V] HAI VINTO! | " << unknown << endl;
+			break;
+		}
+	}
+	if (word != unknown)
+	{
+		cout << "[X] HAI PERSO! | " << word << endl;
 	}
 
-	myfile << "caca\n";
-	myfile << "popo\n";
-	myfile.close();
+//	ofstream myfile ("/home/tommaso/info/tpsit/gioco_impiccato/a.txt", ios::app);
+//	if (!myfile.is_open())
+//	{
+//		cout << "Percorso non valido" << endl;
+//		return 1;
+//	}
+//
+//	myfile << "caca\n";
+//	myfile << "popo\n";
+//	myfile.close();
 
 	return 0;
 }
